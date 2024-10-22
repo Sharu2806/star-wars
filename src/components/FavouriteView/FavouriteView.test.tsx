db1b31e8dccc54a9b153e89e984594cc82fa8228
Sharu2806 <jagtap.sharayu03@yahoo.com>
2024-10-23 00:12:51 +0100

    Update FavouriteView.test.tsx

--- src/components/FavouriteView/FavouriteView.test.tsx
+++ src/components/FavouriteView/FavouriteView.test.tsx
@@ -1,27 +1,52 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import FavouriteView from "./FavouriteView";
+import { FavListContext } from '../App/App';
 
 describe("Favourites List Page", () => {
 
-    test("Should render favourites List if added", async () => {
-      
+    test("Should render warning message if favourite character is not added", () => {
+      const favListContextValue = {
+        favList: [],
+        addFavList: jest.fn(),
+        removeFavList: jest.fn()
+      };
       const renderComponent = () => render(
         <MemoryRouter>
-          <FavouriteView />
+          <FavListContext.Provider value={favListContextValue}>
+            <FavouriteView />
+          </FavListContext.Provider>
         </MemoryRouter>
       );
       renderComponent();
-      
-      expect(screen.getByAltText('spinner')).toBeInTheDocument();
-      await waitFor(() => {
-        expect(screen.queryByAltText('spinner')).not.toBeInTheDocument();
-      });
-  
-      await waitFor(() => {
-        expect(screen.getAllByText('Show Details').length).toEqual(2);
-      });
+      expect(screen.getByText('No Character added to favourites')).toBeInTheDocument();
+    });
+
+    test("Should render list if favourite characters are added", () => {
+      const favListContextValue = {
+        favList: [{
+          name: 'Joe Doe',
+          gender: 'Male',
+          Planet: 'Earth',
+          },
+          {
+            name: 'Judy Doe',
+            gender: 'Female',
+            Planet: 'Earth',
+          }],
+        addFavList: jest.fn(),
+        removeFavList: jest.fn()
+      };
+      const renderComponent = () => render(
+        <MemoryRouter>
+          <FavListContext.Provider value={favListContextValue}>
+            <FavouriteView />
+          </FavListContext.Provider>
+        </MemoryRouter>
+      );
+      renderComponent();
+      expect(screen.getAllByText('Delete').length).toBe(2);
     });
 });  
